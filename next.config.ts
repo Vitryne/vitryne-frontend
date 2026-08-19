9890b3a779b6b5326b68cc2bc2f09275d948a60b
Henrique Pacheco Alves <henriquepachecoalves7@gmail.com>
2026-08-18 23:05:06 -0300

    ETI-170 feat: criando configuracoes do projeto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects(){
+    return[
+      {
+        source: "/",
+        destination: "/login",
+        permanent: false,
+      }
+    ]
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
